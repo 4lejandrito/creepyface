c103b50fe7c8ac861f131fc8f183321e99b0646c
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-08-10 23:32:40 +0200

    Removing webpack

--- rollup.config.js
+++ rollup.config.js
@@ -4,34 +4,53 @@ import postcss from 'rollup-plugin-postcss';
 import commonjs from 'rollup-plugin-commonjs';
 import uglify from 'rollup-plugin-uglify';
 import replace from 'rollup-plugin-replace';
+import pkg from './package.json';
 
 const production = process.env.NODE_ENV === 'production';
 
-export default {
-    entry : 'src/index.js',
-    dest : 'public/bundle-rollup.js',
-    format : 'iife',
-    plugins : [
-        postcss({extensions: ['.css']}),
-        resolve(),
-        commonjs(),
-        babel({
-            babelrc: false,
-            exclude: 'node_modules/**',
-            presets: [
-                [
-                    'es2015', {
-                        modules: false
-                    }
-                ],
-                'stage-0',
-                'react'
+const compilePlugins = [
+    postcss({extensions: ['.css']}),
+    babel({
+        babelrc: false,
+        exclude: 'node_modules/**',
+        presets: [
+            [
+                'es2015', {
+                    modules: false
+                }
             ],
-            plugins: ['external-helpers']
-        }),
-        replace({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-        }),
-        production && uglify()
-    ]
-};
+            'stage-0',
+            'react'
+        ],
+        plugins: ['external-helpers']
+    })
+];
+
+export default[
+    {
+        entry : 'src/index.js',
+        dest : pkg.browser,
+        format : 'umd',
+        plugins : compilePlugins.concat([
+            resolve(),
+            commonjs(),
+            replace({
+                'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+            }),
+            production && uglify()
+        ])
+    }, {
+        entry : 'src/index.js',
+        external : Object.keys(pkg.dependencies),
+        plugins : compilePlugins,
+        targets : [
+            {
+                dest: pkg.main,
+                format: 'cjs'
+            }, {
+                dest: pkg.module,
+                format: 'es'
+            }
+        ]
+    }
+];
