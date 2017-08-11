81ca615fa99fdaa8a7cce510e62258067e460022
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-08-11 18:07:12 +0200

    Simplifying streams

--- src/streams/combined.js
+++ src/streams/combined.js
@@ -3,7 +3,7 @@ import Observable from 'zen-observable';
 export default function combined(streams) {
     return new Observable(observer => {
         let subscriptions = streams.map(
-            stream => stream().subscribe(p => observer.next(p))
+            stream => stream.subscribe(p => observer.next(p))
         );
         return () => subscriptions.map(s => s.unsubscribe());
     });
--- src/streams/mouse.js
+++ src/streams/mouse.js
@@ -1,8 +1,6 @@
 import point from './util/point';
 import events from './util/events';
 
-export default function mousePoints() {
-    return events(document, 'mousemove').map(
-        event => point(event.pageX, event.pageY, event.target, 'mouse')
-    );
-}
+export default events(document, 'mousemove').map(
+    event => point(event.pageX, event.pageY, event.target, 'mouse')
+);
