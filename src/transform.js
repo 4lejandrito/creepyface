a856e0bc6cd7e8ef943c33b02ae2a9d3a33e7361
Alejandro Tardín <alejandro@tardin.com>
2017-08-14 02:41:41 +0200

    Usign algebra functions

--- src/transform.js
+++ src/transform.js
@@ -1,30 +1,15 @@
-function rotate(v, deg) {
-    return {
-        x: v.x * Math.cos(deg) - v.y * Math.sin(deg),
-        y: v.x * Math.sin(deg) + v.y * Math.cos(deg)
-    };
-}
+import {rotate, getAngle, diff} from './streams/util/algebra';
 
-function getAngle(node, x, y, chunks) {
-    const center = {
-        x: node.offsetLeft + node.clientWidth / 2,
-        y: node.offsetTop + node.clientHeight / 2
-    }, v = rotate({
-        x: x - center.x,
-        y: y - center.y
-    }, Math.PI / chunks);
-
-    let angle = Math.atan2(v.y, v.x);
-    if (angle < 0) angle += 2 * Math.PI;
-    return angle * 180 / Math.PI;
-}
+const center = node => [
+    node.offsetLeft + node.clientWidth / 2,
+    node.offsetTop + node.clientHeight / 2
+];
 
 export default function transform(point, node, pictures) {
     let {target, source, coords} = point,
-        [x, y] = coords,
         {move, hover} = pictures,
         chunks = move.length,
-        angle = getAngle(node, x, y, chunks),
+        angle = getAngle(rotate(diff(coords, center(node)), 180 / chunks)),
         number = Math.floor((angle / 360) * chunks),
         picture = (node === target && hover) ? hover : move[number];
 
@@ -33,7 +18,7 @@ export default function transform(point, node, pictures) {
     }
 
     if (process.env.NODE_ENV !== 'production') {
-        console.log(`${source} => ${x}, ${y} => ${angle} => ${number} => ${picture}`);
+        console.log(`${source} => ${coords} => ${angle} => ${number} => ${picture}`);
     }
 
     return picture;
