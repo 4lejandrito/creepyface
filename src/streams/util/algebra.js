85026390e440d4ef554f091a2bb06ae6e8414c76
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-08-14 02:22:48 +0200

    Extracting algebra functions

---
+++ src/streams/util/algebra.js
@@ -0,0 +1,23 @@
+export function rotate(v, deg) {
+    let rad = deg * Math.PI / 180;
+    return [
+        v[0] * Math.cos(rad) - v[1] * Math.sin(rad),
+        v[0] * Math.sin(rad) + v[1] * Math.cos(rad)
+    ];
+}
+
+export function getAngle(v) {
+    let angle = Math.atan2(v[1], v[0]);
+    if (angle < 0) angle += 2 * Math.PI;
+    return angle * 180 / Math.PI;
+}
+
+export let rand = x => Math.floor(Math.random() * x);
+export let sum = (x, y) => x + y;
+export let square = x => x * x;
+export let norm = v => Math.sqrt(v.map(square).reduce(sum, 0));
+export let unit = v => v.map(x => x / norm(v));
+export let diff = (v1, v2) => v1.map((x, i) => x - v2[i]);
+export let times = (v, n) => v.map(x => x * n);
+export let add = (v1, v2) => v1.map((x, i) => x + v2[i]);
+export let sign = n => n ? n < 0 ? -1 : 1 : 0;
