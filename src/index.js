4c69a74f22589f4769a9b9f20515ce3018fec4d6
Alejandro Tardin <alejandro@tardin.com>
2017-08-17 16:21:42 +0200

    Improving API. Now the image will be chosen by proximity to its angle

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import mousePoints from './streams/mouse';
 import fingerPoints from './streams/finger';
 import combined from './streams/combined';
 import toJs from 'data-attrs-to-js';
+import WindRose from 'windrose';
+import {rad} from './streams/util/algebra';
 
 const defaultPoints = combined([mousePoints, fingerPoints]);
-const toArray = o => Object.keys(o).sort().map(key => o[key]);
+const toArray = o => Object.keys(o).map(key => ({angle: getAngle(key), src: o[key]}));
+const getAngle = key => rad(WindRose.getDegrees(key.toUpperCase()).value);
 
 export default function creepyFace(element, pictures, points = defaultPoints) {
     if (!pictures) pictures = getPictureData(element);
--- src/transform.js
+++ src/transform.js
@@ -1,24 +1,26 @@
-import {rotate, getAngle, diff} from './streams/util/algebra';
+import {rotate, getAngle, diff, sign} from './streams/util/algebra';
 
 const center = node => [
     node.offsetLeft + node.clientWidth / 2,
     node.offsetTop + node.clientHeight / 2
 ];
 
+let shortest = angle => Math.abs(angle) > Math.PI ? angle - sign(angle) * 2 * Math.PI : angle;
+let compare = angle => (a, b) => Math.abs(shortest(a.angle - angle)) - Math.abs(shortest(b.angle - angle))
+let closest = (angle, slices) => slices.slice(0).sort(compare(angle))[0];
+
 export default function transform(point, node, pictures) {
     let {target, source, coords} = point,
         {slices, hover} = pictures,
-        chunks = slices.length,
-        angle = getAngle(rotate(diff(coords, center(node)), Math.PI / chunks)),
-        number = Math.floor((angle / (2 * Math.PI)) * chunks),
-        picture = (node === target && hover) ? hover : slices[number];
+        angle = getAngle(rotate(diff(coords, center(node)), Math.PI / 2)),
+        picture = (node === target && hover) ? hover : closest(angle, slices).src;
 
     if (!picture) {
         picture = pictures.default;
     }
 
     if (process.env.NODE_ENV !== 'production') {
-        console.log(`${source} => ${coords} => ${angle} => ${number} => ${picture}`);
+        console.log(`${source} => ${coords} => ${angle} => ${picture}`);
     }
 
     return picture;
