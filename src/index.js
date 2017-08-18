13cdd4b29553460fc04cafabe6c34043141fc8b4
Alejandro Tardin <alejandro@tardin.com>
2017-08-18 17:50:43 +0200

    Extracting function to its own file

--- src/index.js
+++ src/index.js
@@ -3,31 +3,17 @@ import preload from './preload';
 import mousePoints from './streams/mouse';
 import fingerPoints from './streams/finger';
 import combined from './streams/combined';
-import toJs from 'data-attrs-to-js';
-import WindRose from 'windrose';
-import {rad} from './streams/util/algebra';
+import fromElement from './options';
 
 const defaultPoints = combined([mousePoints, fingerPoints]);
-const toArray = o => Object.keys(o).map(key => ({angle: getAngle(key), src: o[key]}));
-const getAngle = key => rad(WindRose.getDegrees(key.toUpperCase()).value);
 
 export default function creepyFace(element, pictures, points = defaultPoints) {
-    if (!pictures) pictures = getPictureData(element);
+    if (!pictures) pictures = fromElement(element);
     return preload(pictures).then(imgs => (
         points.subscribe(point => {
             element.src = imgs[transform(point, element, pictures)].src;
         })
     ));
-}
-
-function getPictureData(element) {
-    let {hover, slice} = toJs(element).src;
-
-    return {
-        default: element.getAttribute('src'),
-        hover,
-        slices: toArray(slice)
-    };
 }
 
 [].forEach.call(document.querySelectorAll('[data-creepy]'), node => (
