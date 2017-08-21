c48203a5c7fdb39a2026abb457b3bc73564f0bec
Alejandro Tardin <alejandro@tardin.com>
2017-08-21 18:22:16 +0200

    Using arrow functions

--- src/util/algebra.js
+++ src/util/algebra.js
@@ -1,16 +1,3 @@
-export function rotate(v, rad) {
-    return [
-        v[0] * Math.cos(rad) - v[1] * Math.sin(rad),
-        v[0] * Math.sin(rad) + v[1] * Math.cos(rad)
-    ];
-}
-
-export function getAngle(v) {
-    let angle = Math.atan2(v[1], v[0]);
-    if (angle < 0) angle += 2 * Math.PI;
-    return angle;
-}
-
 export let rand = x => Math.floor(Math.random() * x);
 export let sum = (x, y) => x + y;
 export let square = x => x * x;
@@ -22,3 +9,8 @@ export let add = (v1, v2) => v1.map((x, i) => x + v2[i]);
 export let sign = n => n ? n < 0 ? -1 : 1 : 0;
 export let rad = deg => deg * Math.PI / 180;
 export let mod = (n, m) => (m + n % m) % m;
+export let getAngle = v => mod(Math.atan2(v[1], v[0]), 2 * Math.PI);
+export let rotate = (v, rad) => [
+    v[0] * Math.cos(rad) - v[1] * Math.sin(rad),
+    v[0] * Math.sin(rad) + v[1] * Math.cos(rad)
+];
