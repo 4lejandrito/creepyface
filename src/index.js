6571216ab8cfbab25f2085746067622b2f419db2
Alejandro Tardín <alejandro@tardin.com>
2017-08-23 20:40:22 +0200

    Explicitly avoiding preloaded images to be garbage collected by making them reachable from the original image
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Memory_Management

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import defaults from 'lodash.defaults';
 export default function creepyFace(img, userOptions) {
     const options = defaults({}, userOptions, fromElement(img));
 
-    return loadImages(getSrcs(options)).then(imgs => (
-        options.points.subscribe(
-            point => img.src = imgs && pointToSrc(point, img, options)
-        )
-    ));
+    return loadImages(getSrcs(options)).then(imgs => {
+        img.creepyFaceReachableImages = imgs;
+        return options.points.subscribe(
+            point => img.src = pointToSrc(point, img, options)
+        );
+    });
 }
 
 $('img[data-creepy]').forEach(img => creepyFace(img));
