056a1d6734d3c215a52722bc5a659ca5ca45e67c
Alejandro Tardín <alejandro@tardin.com>
2017-08-24 08:43:01 +0200

    Making throttle configurable at the top level

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import loadImages from 'image-promise';
 import {fromElement, getSrcs} from './options';
 import $ from 'queryselectorall';
 import defaults from 'object.defaults';
+import throttle from 'throttleit';
 
 export default function creepyFace(img, userOptions) {
     const options = defaults({}, userOptions, fromElement(img));
@@ -10,7 +11,10 @@ export default function creepyFace(img, userOptions) {
     return loadImages(getSrcs(options)).then(imgs => {
         img.creepyFaceReachableImages = imgs;
         return options.points.map(
-            point => img.src = pointToSrc(point, img, options)
+            throttle(
+                point => img.src = pointToSrc(point, img, options),
+                options.throttle
+            )
         );
     });
 }
