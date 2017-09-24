79d17c685a12539f86d7430dc50970f4d2cddf90
Alejandro Tardin <alejandro@tardin.com>
2017-09-25 00:01:58 +0200

    Using coords to detect hover

--- src/point-to-src.js
+++ src/point-to-src.js
@@ -9,14 +9,18 @@ const center = node => {
 const shortest = angle => Math.abs(angle) > Math.PI ? angle - sign(angle) * 2 * Math.PI : angle
 const compare = angle => (a, b) => Math.abs(shortest(a.angle - angle)) - Math.abs(shortest(b.angle - angle))
 const closest = (angle, looks) => looks.slice(0).sort(compare(angle))[0]
+const within = (n, a, b) => n >= a && n <= b
+const contains = ({left, top, right, bottom}, [x, y]) => (
+  within(x, left, right) && within(y, top, bottom)
+)
 
 export default function pointToSrc (point, img, options) {
-  const {target, coords} = point
+  const {coords} = point
   const {looks, hover, fieldOfVision} = options
   const angle = getAngle(rotate(diff(coords, center(img)), Math.PI / 2))
   let src = options.default
 
-  if (img === target && hover) {
+  if (contains(img.getBoundingClientRect(), coords)) {
     src = hover
   } else {
     const closestLook = closest(angle, looks)
