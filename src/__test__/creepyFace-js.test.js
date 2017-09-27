191f876d5498aa815611b5cab938c70923b7bb4b
Alejandro Tardín <alejandro@tardin.com>
2017-09-27 07:18:19 +0200

    Fixing indentation

--- src/__test__/creepyFace-js.test.js
+++ src/__test__/creepyFace-js.test.js
@@ -13,14 +13,14 @@ describe('Using JS api', () => {
     throttle: 100,
     hover: 'hoverUrl',
     looks: [
-            {angle: 0 * Math.PI / 4, src: 'northUrl'},
-            {angle: 1 * Math.PI / 4, src: 'northEastUrl'},
-            {angle: 2 * Math.PI / 4, src: 'eastUrl'},
-            {angle: 3 * Math.PI / 4, src: 'southEastUrl'},
-            {angle: 4 * Math.PI / 4, src: 'southUrl'},
-            {angle: 5 * Math.PI / 4, src: 'southWestUrl'},
-            {angle: 6 * Math.PI / 4, src: 'westUrl'},
-            {angle: 7 * Math.PI / 4, src: 'northWestUrl'}
+      {angle: 0 * Math.PI / 4, src: 'northUrl'},
+      {angle: 1 * Math.PI / 4, src: 'northEastUrl'},
+      {angle: 2 * Math.PI / 4, src: 'eastUrl'},
+      {angle: 3 * Math.PI / 4, src: 'southEastUrl'},
+      {angle: 4 * Math.PI / 4, src: 'southUrl'},
+      {angle: 5 * Math.PI / 4, src: 'southWestUrl'},
+      {angle: 6 * Math.PI / 4, src: 'westUrl'},
+      {angle: 7 * Math.PI / 4, src: 'northWestUrl'}
     ]
   })
 
