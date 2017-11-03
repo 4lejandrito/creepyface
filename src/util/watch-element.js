6e3c9b48c47594de24d7a47570c6154d46d2abff
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-11-03 01:35:39 +0100

    Supporting tests

--- src/util/watch-element.js
+++ src/util/watch-element.js
@@ -1,6 +1,7 @@
 export default (node, onAdded, onRemoved) => {
   let wasAdded = !!node.parentNode
   if (wasAdded) onAdded()
+  if (!window.MutationObserver) return () => {}
   const observer = new window.MutationObserver(mutations => {
     if (!node.parentNode && wasAdded) {
       onRemoved()
