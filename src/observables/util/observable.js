0d0752b16cc69a9c3a814f86ad494ed1abba8edb
Alejandro Tardin <alejandro@tardin.com>
2017-11-06 07:49:03 +0100

    Using my own simple observable

--- src/observables/util/observable.js
+++ src/observables/util/observable.js
@@ -1,2 +1,17 @@
-import Observable from 'zen-observable'
-export default Observable
+export default class Observable {
+  constructor (subscriber) {
+    this.subscriber = subscriber
+  }
+  subscribe (consumer) {
+    const observer = {next: consumer}
+    const result = this.subscriber(observer)
+    return typeof result === 'function' ? { unsubscribe: result } : result
+  }
+  map (transform) {
+    return new Observable(observer => (
+      this.subscribe(
+        value => observer.next(transform(value))
+      )
+    ))
+  }
+}
