6afc16e00fa908343638824d7009934c97fb1b13
Alejandro Tardin <alejandro@tardin.com>
2017-11-09 10:27:12 +0100

    Renaming sources observable

--- src/util/attach.js
+++ src/util/attach.js
@@ -2,7 +2,7 @@
 /* global Image */
 import preload from './preload'
 import getOptions from './options'
-import sources from '../observables/sources'
+import creepy from '../observables/creepy'
 import type {UserOptions} from './options'
 import type {Cancel} from './types'
 
@@ -10,7 +10,7 @@ export default (img: Image, userOptions?: UserOptions): Cancel => {
   const options = getOptions(img, userOptions)
   const setSrc = src => { img.src = src }
   const subscribed = preload(img, options).then(() => (
-    sources(img, options).subscribe(data => {
+    creepy(img, options).subscribe(data => {
       setSrc(data.src)
       options.debug(data)
     })
