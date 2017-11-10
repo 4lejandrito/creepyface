7cde450b794262a9086a992b4ba86cac2db55926
Alejandro Tardin <alejandro@tardin.com>
2017-11-10 08:43:18 +0100

    Adding lolex type definitions

---
+++ flow-typed/npm/lolex_vx.x.x.js
@@ -0,0 +1,12 @@
+// flow-typed signature: 5a6740a7876cef2cb2bedab70497b156
+// flow-typed version: <<STUB>>/lolex_v^2.1.2/flow_v0.58.0
+
+declare type Clock = {
+  tick: (millis: number) => void
+}
+
+declare module 'lolex' {
+  declare export default {
+    install: void => Clock
+  }
+}
