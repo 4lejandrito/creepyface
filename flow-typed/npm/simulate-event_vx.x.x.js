8387a3bbd9443e0fc3ab08edf5e88cf2ca576e7a
Alejandro Tardin <alejandro@tardin.com>
2017-11-11 01:38:35 +0100

    Adding type definitions for simulate-event

---
+++ flow-typed/npm/simulate-event_vx.x.x.js
@@ -0,0 +1,8 @@
+// flow-typed signature: 4e346d7050d703948e39acc814f2d4d8
+// flow-typed version: <<STUB>>/simulate-event_v^1.4.0/flow_v0.58.0
+
+declare module 'simulate-event' {
+  declare export default {
+    simulate: (target: EventTarget, eventName: string, payload: any) => void
+  }
+}
