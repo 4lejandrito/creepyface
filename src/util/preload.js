e14868217e929367f6f434bede3d991d9d50fbf2
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-12-05 08:02:56 +0100

    This seems unnecessary now

--- src/util/preload.js
+++ src/util/preload.js
@@ -1,21 +1,8 @@
 // @flow
 /* global HTMLImageElement */
 import loadImages from 'image-promise'
-import isFirefox from 'is-firefox'
 import type {Options, ImageURL} from './options'
 import type {CreepyImage} from './types'
-
-function showAndHideImages (imgs: Array<HTMLImageElement>) {
-  imgs.forEach(img => {
-    img.style.position = 'fixed'
-    img.style.height = '0px'
-    const body = document.body
-    if (body) {
-      body.appendChild(img)
-      setTimeout(() => body.removeChild(img), 1000)
-    }
-  })
-}
 
 const getSrcs = (options: Options): Array<ImageURL> => {
   const srcs = options.looks.map(({src}) => src)
@@ -27,6 +14,5 @@ const getSrcs = (options: Options): Array<ImageURL> => {
 export default function preload (img: CreepyImage, options: Options): Promise<void> {
   return loadImages(getSrcs(options)).then(imgs => {
     (img: Object).creepyFaceReachableImages = imgs
-    if (isFirefox) showAndHideImages(imgs)
   })
 }
