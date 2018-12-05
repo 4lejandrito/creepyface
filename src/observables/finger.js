7ff07d72074b72d5a549b108758009a4c8c662cc
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-12-05 17:54:37 +0100

    Usign a single event listener for every creepyface

--- src/observables/finger.js
+++ src/observables/finger.js
@@ -3,14 +3,23 @@
 import Observable from './util/observable'
 import {add} from '../util/algebra'
 
-export default new Observable(observer => {
-  const next = (event: TouchEvent): mixed => {
-    let point = [0, 0]
-    for (const touch of event.touches) {
-      point = add(point, [touch.clientX, touch.clientY])
+const observers = []
+
+document.addEventListener(
+  'touchmove',
+  (event: TouchEvent) => observers.forEach(
+    observer => {
+      let point = [0, 0]
+      for (const touch of event.touches) {
+        point = add(point, [touch.clientX, touch.clientY])
+      }
+      observer.next(point)
     }
-    observer.next(point)
-  }
-  document.addEventListener('touchmove', next, true)
-  return () => document.removeEventListener('touchmove', next, true)
+  ),
+  true
+)
+
+export default new Observable(observer => {
+  observers.push(observer)
+  return () => { observers.splice(observers.indexOf(observer), 1) }
 })
