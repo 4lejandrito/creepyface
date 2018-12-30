552821ca661654ece120f711fc98c7fa9ac4ec5c
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-12-30 13:04:53 +0100

    Upgrading lolex

--- flow-typed/npm/lolex_vx.x.x.js
+++ flow-typed/npm/lolex_vx.x.x.js
@@ -2,7 +2,8 @@
 // flow-typed version: <<STUB>>/lolex_v^2.1.2/flow_v0.58.0
 
 declare type Clock = {
-  tick: (millis: number) => void
+  tick: (millis: number) => void,
+  uninstall: void => void
 }
 
 declare module 'lolex' {
