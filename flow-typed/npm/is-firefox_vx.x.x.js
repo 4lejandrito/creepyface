4346fa6b628a5ac3acfa8632509799a4af156f1b
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-12-31 02:40:58 +0100

    No longer needed

--- flow-typed/npm/is-firefox_vx.x.x.js
+++
@@ -1,6 +0,0 @@
-// flow-typed signature: d401c071ea00147eebc87c211ac671f8
-// flow-typed version: <<STUB>>/is-firefox_v^1.0.3/flow_v0.58.0
-
-declare module 'is-firefox' {
-  declare export default boolean
-}
