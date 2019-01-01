f9b4c2907ffd3a185ac2de594c16280cdbf3693c
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-01-01 21:24:14 +0100

    Removing unused code

--- src/util/algebra.js
+++ src/util/algebra.js
@@ -4,13 +4,7 @@ export type Degrees = number
 export type Radians = number
 export type Angle = Degrees
 
-export const rand = (x: number) => Math.floor(Math.random() * x)
-export const sum = (x: number, y: number) => x + y
-export const square = (x: number) => x * x
-export const norm = (v: Vector) => Math.sqrt(v.map(square).reduce(sum, 0))
-export const unit = (v: Vector): Vector => v.map(x => x / norm(v))
 export const diff = (v1: Vector, v2: Vector): Vector => v1.map((x, i) => x - v2[i])
-export const times = (v: Vector, n: number): Vector => v.map(x => x * n)
 export const add = (v1: Vector, v2: Vector): Vector => v1.map((x, i) => x + v2[i])
 export const sign = (n: number) => n ? n < 0 ? -1 : 1 : 0
 export const rad = (deg: Degrees) => deg * Math.PI / 180
