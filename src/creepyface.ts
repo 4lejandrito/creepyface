176d8d7a734efcaa85059f873cd95b8de8103b7f
Alejandro Tardín <alejandro@tardin.com>
2019-01-19 01:37:53 +0100

    Also supporting the old attribute to keep backwards compatibility

--- src/creepyface.ts
+++ src/creepyface.ts
@@ -28,7 +28,9 @@ creepyface.cancel = (img: HTMLImageElement) => {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const elements = document.querySelectorAll('img[data-creepyface]')
+  const elements = document.querySelectorAll(
+    'img[data-creepy],img[data-creepyface]'
+  )
   for (let i = 0; i < elements.length; i++) {
     const el = elements[i]
     if (el instanceof HTMLImageElement) creepyface(el)
