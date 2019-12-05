56452d95cd77cdfbef9eb1ff2de14932eaa67e9c
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-12-05 08:41:00 +0100

    No longer needed

--- src/util/watch-element.ts
+++ src/util/watch-element.ts
@@ -8,10 +8,10 @@ export default (
 ): Cancel => {
   const isReady = (is: boolean) => (is ? onAdded() : onRemoved())
   if (MutationObserver) {
-    let wasInDOM = document.body && document.body.contains(node)
+    let wasInDOM = document.body.contains(node)
     if (wasInDOM) isReady(true)
     const observer = new MutationObserver(mutations => {
-      const isInDOM = document.body && document.body.contains(node)
+      const isInDOM = document.body.contains(node)
       if (!isInDOM && wasInDOM) {
         isReady(false)
         wasInDOM = false
@@ -23,7 +23,7 @@ export default (
     observer.observe(document, { childList: true, subtree: true })
     return () => observer.disconnect()
   } else {
-    if (document.body && document.body.contains(node)) isReady(true)
+    if (document.body.contains(node)) isReady(true)
     node.addEventListener('DOMNodeInserted', () => isReady(true), false)
     node.addEventListener('DOMNodeRemoved', () => isReady(false), false)
     return noop
