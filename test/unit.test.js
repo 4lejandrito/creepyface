84289d48d9861c48f8fdead961d281f30dee2226
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-12-30 01:15:07 +0100

    Allowing more than one provider to be defined

--- test/unit.test.js
+++ test/unit.test.js
@@ -259,6 +259,25 @@ describe('creepyface', () => {
     })
   })
 
+  describe('Using DOM api with several point providers', () => {
+    doTest(img => {
+      img.setAttribute('src', 'http://localhost/serious')
+      img.setAttribute('data-throttle', '100')
+      img.setAttribute('data-points', 'mouse,finger')
+      img.setAttribute('data-src-hover', 'http://localhost/hover')
+      img.setAttribute('data-src-look-0', 'http://localhost/north')
+      img.setAttribute('data-src-look-45', 'http://localhost/northEast')
+      img.setAttribute('data-src-look-90', 'http://localhost/east')
+      img.setAttribute('data-src-look-135', 'http://localhost/southEast')
+      img.setAttribute('data-src-look-180', 'http://localhost/south')
+      img.setAttribute('data-src-look-225', 'http://localhost/southWest')
+      img.setAttribute('data-src-look-270', 'http://localhost/west')
+      img.setAttribute('data-src-look-315', 'http://localhost/northWest')
+
+      return creepyface(img)
+    })
+  })
+
   describe('Using JS api with custom points', () => {
     doTest(img => {
       img.setAttribute('src', 'http://localhost/serious')
