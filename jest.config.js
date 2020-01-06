4edb01bfc1aa562325aa9a2482027ea681de8f99
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-01-06 11:34:47 +0100

    Forcing 100% coverage from now on

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,13 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest'
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js$',
-  moduleFileExtensions: ['ts', 'js']
+  moduleFileExtensions: ['ts', 'js'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 }
