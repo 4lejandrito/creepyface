67d8cd28250fa0d40fac40d4a526d0784c0cc7b2
Alejandro Tardín <alejandro@tardin.com>
2020-03-09 19:31:44 +0100

    No longer used

--- packages/creepyface-site/backend/analytics/index.js
+++ packages/creepyface-site/backend/analytics/index.js
@@ -26,43 +26,4 @@ module.exports = app => {
     track(req.cookies.uuid, type, payload)
     res.sendStatus(200)
   })
-
-  app.get('/analytics', async (req, res) => {
-    const days = parseInt(req.query.days || 7)
-
-    res.send({
-      names: (
-        await all(
-          `select name, count(*) as count from event group by name order by count desc`
-        )
-      ).map(({ name }) => name),
-      dates: (
-        await all(`
-        select
-          Cast ((julianday('now') - julianday(timestamp)) As Integer) as day,
-          name,
-          count(*) as count
-        from event
-        where
-          session not in (
-            select
-              distinct session
-            from event
-            where
-              name = 'request'
-              and json_extract(payload, '$.url') like '%admin%.js'
-          )
-          and day <= ${days - 1}
-        group by day, name
-        order by timestamp asc
-      `)
-      ).reduce(
-        (acc, cur) => ({
-          ...acc,
-          [cur.day]: { ...acc[cur.day], [cur.name]: cur.count }
-        }),
-        {}
-      )
-    })
-  })
 }
