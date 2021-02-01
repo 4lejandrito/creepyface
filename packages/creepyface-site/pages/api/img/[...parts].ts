1f237e8b54c2420ebef9fdbf871f7ca6cd58dcb2
Alejandro Tardín <alejandro@tardin.com>
2021-02-01 20:38:56 +0100

    Simplifying

--- packages/creepyface-site/pages/api/img/[...parts].ts
+++ packages/creepyface-site/pages/api/img/[...parts].ts
@@ -1,88 +1,58 @@
 import { fileRoute, route } from '../../../src/backend/api'
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest } from 'next'
 import resize, { Size } from '../../../src/backend/resize'
 import { uploads } from '../../../src/backend/storage'
-import getCloudinaryURL from '../../../src/backend/cloudinary'
 import prisma from '../../../prisma'
 
-const getImagesPath = (uuid: string) =>
-  uuid === '0'
-    ? 'public/nala'
-    : uuid === 'ray'
-    ? 'public/ray'
-    : `${uploads}/${uuid}/img`
-
-let useCloudinary = false
-
-const sendImage = async (
-  req: NextApiRequest,
-  res: NextApiResponse,
-  options: {
-    uuid: string
-    name: string
-    maxAgeDays?: number
-    size: Size
-    local?: boolean
-  }
-) => {
-  const { uuid, name, maxAgeDays = 1, size, local } = options
-  const path = await resize(`${getImagesPath(uuid)}/${name}.jpeg`, uuid, size)
-  const sendLocalImage = () => {
-    res.setHeader(
-      'Cache-Control',
-      `max-age=${maxAgeDays * 24 * 60 * 60}, immutable`
-    )
-    return fileRoute(path)(req, res)
-  }
-  if (local || !useCloudinary) {
-    return sendLocalImage()
-  }
-  try {
-    res.redirect(await getCloudinaryURL({ uuid, size, name, path }))
-  } catch (err) {
-    console.error(err)
-    sendLocalImage()
-  }
-}
-
-export default route(async (req, res) => {
-  const uuid = req.query.parts[0] ?? '0'
-  const name = (req.query.parts[1] as string) || 'serious'
-  const size = (req.query.parts[2] as Size) || 'medium'
-  const namespace = (req.query.namespace as string) || undefined
+const getUuid = async (req: NextApiRequest) => {
+  let uuid = req.query.parts[0] ?? '0'
 
   if (!uuid.match(/^\d+$/)) {
-    await sendImage(req, res, { uuid, name, size })
-    return
+    return uuid
   }
 
   const i = parseInt(uuid)
+  const namespace = (req.query.namespace as string) || undefined
 
-  if (namespace === 'liferay' && i === 0) {
-    await sendImage(req, res, { uuid: 'ray', name, size })
-    return
-  } else if (i === 0) {
-    await sendImage(req, res, { uuid: '0', name, size })
-    return
+  if (i === 0) {
+    return namespace === 'liferay' ? 'ray' : 'nala'
   }
 
-  const creepyface = await prisma.creepyface.findFirst({
-    where: {
-      canUseAsSample: true,
-      approved: true,
-      exclusive: !namespace ? false : undefined,
-      namespace,
-    },
-    orderBy: {
-      timestamp: 'asc',
-    },
-    skip: i - 1,
-  })
+  return (
+    await prisma.creepyface.findFirst({
+      where: {
+        canUseAsSample: true,
+        approved: true,
+        exclusive: !namespace ? false : undefined,
+        namespace,
+      },
+      orderBy: {
+        timestamp: 'asc',
+      },
+      skip: i - 1,
+    })
+  )?.uuid
+}
 
-  if (!creepyface) {
+export default route(async (req, res) => {
+  const uuid = await getUuid(req)
+
+  if (!uuid) {
     res.status(404).end()
     return
   }
 
-  await sendImage(req, res, { uuid: creepyface.uuid, name, size })
+  res.setHeader('Cache-Control', `max-age=${24 * 60 * 60}, immutable`)
+
+  return fileRoute(
+    await resize(
+      `${
+        uuid === 'nala' || uuid === 'ray'
+          ? `public/${uuid}`
+          : `${uploads}/${uuid}/img`
+      }/${(req.query.parts[1] as string) || 'serious'}.jpeg`,
+      uuid,
+      (req.query.parts[2] as Size) || 'medium'
+    )
+  )(req, res)
 })
