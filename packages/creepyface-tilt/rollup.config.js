3730fe8f70c2502e4f4e52c7ce94a793079fcb84
Alejandro Tardín <alejandro@tardin.com>
2021-02-04 08:28:12 +0100

    Upgrading to official rollup plugins

--- packages/creepyface-tilt/rollup.config.js
+++ packages/creepyface-tilt/rollup.config.js
@@ -1,7 +1,7 @@
 import pkg from './package.json'
 import resolve from '@rollup/plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import babel from 'rollup-plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import babel from '@rollup/plugin-babel'
 import { uglify } from 'rollup-plugin-uglify'
 
 const extensions = ['.js', '.ts']
