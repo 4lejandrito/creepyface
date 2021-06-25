9b6f5c37f181867ac50aa8b8559e6ff421690b95
Alejandro Tardín <alejandro@tardin.com>
2021-06-25 08:46:13 +0200

    Upgrading deps

---
+++ packages/creepyface-site/@types/index.d.ts
@@ -0,0 +1,25 @@
+declare module 'clipbrd' {
+  export function copy(text: string): void
+}
+
+declare module 'dataurl-to-blob' {
+  export default function dataURLToBlob(dataURL: string): Blob
+}
+
+declare module 'stop-media-stream' {
+  export default function stopMediaStream(stream: MediaStream): void
+}
+
+declare module 'no-bounce' {
+  export default function noBounce(): void
+}
+
+declare module '*.mp3' {
+  const content: string
+  export default content
+}
+
+declare module '*.hbs' {
+  declare function template(context: any, options?: any): string
+  export = template
+}
--- packages/creepyface-site/next.config.js
+++ packages/creepyface-site/next.config.js
@@ -1,64 +1,62 @@
 const { withPlausibleProxy } = require('next-plausible')
 
-module.exports = (nextConfig) => withPlausibleProxy()(({
-  ...nextConfig,
-  future: {
-    webpack5: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/img/:slug*',
-        destination: '/api/img/:slug*',
-        permanent: true,
-      },
-      {
-        source: '/:uuid/download',
-        destination: '/api/:uuid/download',
-        permanent: true,
-      },
-      {
-        source: '/api/:uuid/download',
-        destination: '/api/content/:uuid/creepyface.zip',
-        permanent: true,
-      },
-      {
-        source: '/content/:slug*',
-        destination: '/api/content/:slug*',
-        permanent: true,
-      },
-      {
-        source: '/creepyface.js',
-        destination: '/api/creepyface.js',
-        permanent: true,
-      },
-      {
-        source: '/creepyface-firefly.js',
-        destination: '/api/creepyface-firefly.js',
-        permanent: true,
-      },
-    ]
-  },
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.externals = {
-        creepyface: 'creepyface',
-      }
-    }
-    config.module.rules.push({
-      test: /\.(mp3)$/,
-      use: [
+module.exports = (nextConfig) =>
+  withPlausibleProxy()({
+    ...nextConfig,
+    async redirects() {
+      return [
         {
-          loader: 'file-loader',
-          options: {
-            publicPath: `${nextConfig.assetPrefix || ''}/_next/static/mp3/`,
-            outputPath: `static/mp3/`,
-            name: '[name]-[hash].[ext]',
-          },
+          source: '/img/:slug*',
+          destination: '/api/img/:slug*',
+          permanent: true,
         },
-      ],
-    })
-    config.module.rules.push({ test: /\.hbs$/, loader: 'handlebars-loader' })
-    return config
-  },
-}))
+        {
+          source: '/:uuid/download',
+          destination: '/api/:uuid/download',
+          permanent: true,
+        },
+        {
+          source: '/api/:uuid/download',
+          destination: '/api/content/:uuid/creepyface.zip',
+          permanent: true,
+        },
+        {
+          source: '/content/:slug*',
+          destination: '/api/content/:slug*',
+          permanent: true,
+        },
+        {
+          source: '/creepyface.js',
+          destination: '/api/creepyface.js',
+          permanent: true,
+        },
+        {
+          source: '/creepyface-firefly.js',
+          destination: '/api/creepyface-firefly.js',
+          permanent: true,
+        },
+      ]
+    },
+    webpack(config, { isServer }) {
+      if (!isServer) {
+        config.externals = {
+          creepyface: 'creepyface',
+        }
+      }
+      config.module.rules.push({
+        test: /\.(mp3)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: `${nextConfig.assetPrefix || ''}/_next/static/mp3/`,
+              outputPath: `static/mp3/`,
+              name: '[name]-[hash].[ext]',
+            },
+          },
+        ],
+      })
+      config.module.rules.push({ test: /\.hbs$/, loader: 'handlebars-loader' })
+      return config
+    },
+  })
